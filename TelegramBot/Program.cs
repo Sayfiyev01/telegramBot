16cc77ec3e9a09c996c24603dd1c79404ffa36d9
Sayfiyev01 <sayfiyevfayozjon01@gmail.com>
2022-07-23 23:41:26 +0500

    something

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("finished");
+Console.WriteLine("goodbye soleniya!");
+Console.WriteLine("something");
 //master changed to test branchyhybyh
 //all codes confirm
