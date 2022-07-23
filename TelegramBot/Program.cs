850561a74512c09bb704b481d4a47f7a6022a972
Sayfiyev01 <sayfiyevfayozjon01@gmail.com>
2022-07-23 23:44:36 +0500

    simon

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("goodbye soleniya!");
+Console.WriteLine("hello simon!");
 Console.WriteLine("something");
 //master changed to test branchyhybyh
 //all codes confirm
